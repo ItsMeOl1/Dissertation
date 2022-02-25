fed5076e933f51214b762e602d0d53a37702d27b
Oliver Hamilton <ohamilton783@gmail.com>
2022-02-25 22:49:25 +0000

    Version 1 - Basic path finding

---
+++ pathfinder.py
@@ -0,0 +1,159 @@
+from warnings import warn
+import heapq #used to create heap structures
+
+class Node:
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0 #how far from start
+        self.h = 0 #using Pythagoras to find a heuristic 
+        self.f = 0 #final
+
+    def __eq__(self, other): #is this node equal to another
+        return self.position == other.position
+    
+    def __repr__(self):
+      return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
+
+    def __lt__(self, other): #less than
+      return self.f < other.f
+    
+    def __gt__(self, other): #greater than
+      return self.f > other.f
+
+    def set_Values(self, g, h):
+        self.g = g
+        self.h = h
+        self.f = self.g + self.h
+
+def return_path(current_node):
+    path = []
+    current = current_node
+    while current is not None:
+        path.append(current.position)
+        current = current.parent
+    return path[::-1]  # Return reversed path
+
+
+def astar(maze, start, end, allow_diagonal_movement = False):
+    """
+    Returns a list of tuples as a path from the given start to the given end in the given maze
+    :param maze:
+    :param start:
+    :param end:
+    :return:
+    """
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.set_Values(0,0)
+    end_node = Node(None, end)
+    end_node.set_Values(0,0)
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Heapify the open_list and Add the start node
+    heapq.heapify(open_list) 
+    heapq.heappush(open_list, start_node)
+
+
+    # what squares do we search
+    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0))
+
+    # Loop until you find the end
+    while len(open_list) > 0: 
+        
+        # Get the current node
+        current_node = heapq.heappop(open_list)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            return return_path(current_node)
+
+        # Generate children
+        children = []
+        
+        for new_position in adjacent_squares: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            # Child is on the closed list
+            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
+                continue
+
+            # Add the child to the open list
+            heapq.heappush(open_list, child)
+
+    warn("Couldn't get a path to destination")
+    return None
+
+def example(print_maze = True):
+
+    maze = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,] * 2,
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,] * 2,
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,] * 2,
+            [0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,] * 2,
+            [0,0,0,1,1,0,0,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,] * 2,
+            [0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,] * 2,
+            [0,0,0,1,0,1,1,1,1,0,1,1,0,0,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,] * 2,
+            [0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,1,1,0,1,0,0,0,0,0,0,1,1,1,0,] * 2,
+            [0,0,0,1,0,1,1,0,1,1,0,1,1,1,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,0,] * 2,
+            [0,0,0,1,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,1,1,] * 2,
+            [0,0,0,1,0,1,0,1,1,0,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,0,1,0,0,0,] * 2,
+            [0,0,0,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,1,0,] * 2,
+            [0,0,0,1,0,1,1,1,1,0,1,0,0,1,1,1,0,1,1,1,1,0,1,1,1,0,1,0,0,0,] * 2,
+            [0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,] * 2,
+            [0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,] * 2,
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,] * 2,]
+    
+    start = (0, 0)
+    end = (len(maze)-1, len(maze[0])-1)
+
+    path = astar(maze, start, end)
+
+    if print_maze:
+      for step in path:
+        maze[step[0]][step[1]] = 2
+      
+      for row in maze:
+        line = []
+        for col in row:
+          if col == 1:
+            line.append("\u2588")
+          elif col == 0:
+            line.append(" ")
+          elif col == 2:
+            line.append(".")
+        print("".join(line))
+
+    print(path)
---
+++ test.py
@@ -0,0 +1,100 @@
+import sys, pygame, pathfinder
+pygame.init()
+
+screenSize = 1600, 900 
+squareSize = 50 #32x18
+dark_gray = 155, 155, 155
+light_gray = 200,200,200
+black = 0,0,0
+red = 255,0,0
+green = 0,255,0
+RUNNING = True
+screen = pygame.display.set_mode(screenSize)
+
+wall = pygame.image.load("wall.png")
+wall = pygame.transform.scale(wall, (50,50))
+
+def drawOutlines(screen, colour, squareSize, screenSize):
+    for x in range(squareSize, screenSize[0], squareSize):
+        pygame.draw.line(screen, colour, (x, 0), (x, screenSize[1]))
+    for y in range(squareSize, screenSize[1], squareSize):
+        pygame.draw.line(screen, colour, (0, y), (screenSize[0], y))
+
+class Map:
+    def __init__(self, x, y):
+        self.max_X = x-1
+        self.max_Y = y-1
+        self.start = 0,0
+        self.end = self.max_X,self.max_Y
+
+        self.grid = [[0 for i in range(y)] for j in range(x)]
+    
+    def set_start(self, x, y):
+        if x <= self.max_X & y <= self.max_Y & x >= 0 & y >= 0:
+            self.start = x,y
+        else:
+            print("INVALID start set")
+
+    def set_end(self, x, y):
+        if x <= self.max_X & y <= self.max_Y:
+            self.end = x,y
+        else:
+            print("INVALID end set")
+
+    def place(self, x, y):
+        self.grid[x][y] = 1
+
+    def clear(self, x, y):
+        self.grid[x][y] = 0
+
+    def drawTiles(self, screen, squareSize):
+        for x in range(len(self.grid)):
+            for y in range(len(self.grid[x])):
+                if self.grid[x][y] == 1:
+                    screen.blit(wall, (x*squareSize, y*squareSize))
+
+    def findPath(self):
+        return pathfinder.astar(self.grid, self.start, self.end)
+
+    def drawPath(self, screen, colour, squareSize):
+        path = self.findPath()
+        for i in range(len(path)-1):
+            start = (path[i][0] * squareSize + squareSize/2, path[i][1] * squareSize + squareSize/2)
+            end = (path[i+1][0] * squareSize + squareSize/2, path[i+1][1] * squareSize + squareSize/2)
+            pygame.draw.line(screen, colour, start, end)
+
+
+map = Map(int(screenSize[0]/squareSize), int(screenSize[1]/squareSize))
+
+while RUNNING:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            RUNNING = False
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                map.findPath()
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1: #if left click
+                map.place(int(event.pos[0]/squareSize), int(event.pos[1]/squareSize))
+            elif event.button == 3: #if right click
+                map.clear(int(event.pos[0]/squareSize), int(event.pos[1]/squareSize))
+
+    if RUNNING:
+        screen.fill(dark_gray)
+        for x in range(0,screenSize[0],squareSize*2):
+            for y in range(0,screenSize[1],squareSize*2):
+                pygame.draw.rect(screen, light_gray , (x, y, squareSize, squareSize))
+        for x in range(squareSize,screenSize[0],squareSize*2):
+            for y in range(squareSize,screenSize[1],squareSize*2):
+                pygame.draw.rect(screen, light_gray , (x, y, squareSize, squareSize))
+        pos = pygame.mouse.get_pos()
+        pygame.draw.rect(screen, red, (int(pos[0]/squareSize)*squareSize, int(pos[1]/squareSize)*squareSize, squareSize, squareSize))
+        #screen.blit(ball, ballrect)
+
+        map.drawTiles(screen,squareSize)
+        drawOutlines(screen, black, squareSize, screenSize)
+        map.drawPath(screen, green, squareSize)
+        pygame.display.flip()
