257109bf6355e8b73af83422b8ef62d50ffbb563
Oliver Hamilton <ohamilton783@gmail.com>
2022-05-09 00:03:22 +0100

    Final Code Cleanup

--- rounds.py
+++ rounds.py
@@ -1,15 +1,17 @@
 import csv
 
+
 class Round:
     roundNo = 0
+
     def __init__(self, delay, buildDelay, name, enemies):
-        Round.roundNo += 1
-        self.delay = int(delay)
+        Round.roundNo += 1  # What round is it
+        self.delay = int(delay)  # Time between enemies
         self.enemyList = enemies
-        self.buildTime = int(buildDelay)
+        self.buildTime = int(buildDelay)  # Delay before round starts
         self.name = name
         self.nextEnemy = self.buildTime
-    
+
     def update(self, ticks):
         self.nextEnemy -= ticks
         if self.nextEnemy < 1 and len(self.enemyList) > 0:
@@ -18,14 +20,16 @@ class Round:
         else:
             return None
 
-def get_next_round():
+
+def get_next_round():  # Get the next round from csv
     with open('rounds.csv', newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for i in range(Round.roundNo + 1):
+        for i in range(Round.roundNo + 1):  # Get to correct line
             csvreader.__next__()
         row = csvreader.__next__()
         delay = row.pop(0)
         buildDelay = row.pop(0)
         name = row.pop(0)
+        # make list out of the remaining fields
         enemyList = [i for i in row if i != '']
     return Round(delay, buildDelay, name, enemyList)
